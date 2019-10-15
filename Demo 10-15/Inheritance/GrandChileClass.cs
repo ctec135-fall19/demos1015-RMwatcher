9f9323cdd65d10bfb2d00cf0ceb4acc33974ac39
Richard <r.mora@students.clark.edu>
2019-10-15 15:24:30 -0700

    lab 10/15 complete

---
+++ Demo 10-15/Inheritance/GrandChileClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class GrandChileClass : ChildClass
+    {
+        // method
+        public void GrandChildMethod()
+        {
+            Console.WriteLine("GrandChildClass.GrandChildMethod");
+        }
+    }
+}
---
+++ Demo 10-15/Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test method inheritance
+            // normally wouldn't use cc or gcc as a class name
+            ChildClass cc = new ChildClass();
+            GrandChileClass gcc = new GrandChileClass();
+
+            Console.WriteLine("Calling method from gcc");
+            gcc.BaseMethod();
+            gcc.ChildMethod();
+            gcc.GrandChildMethod();
+            Console.WriteLine();
+
+            Console.WriteLine("Calling method from cc");
+            cc.BaseMethod();
+            cc.ChildMethod();
+            // cc.GrandChildMethod();
+            Console.WriteLine();
+
+            // test base class fields
+            BaseClass bc = new BaseClass(123, 456);
+            bc.PrintState();
+            Console.WriteLine();
+
+            // test child print state
+            ChildClass cc2 = new ChildClass(987, 654, 345);
+            cc2.PrintState();
+        }
+    }
+}
